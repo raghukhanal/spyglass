994eb44ad2a9c075f06194a7814eb870aa1df7ff
Samantha Lowe <slowe@xpanxion.com>
2021-05-14 09:22:34 -0400

    finished following tutorial, but without the prettiness

--- src/app/components/goal-details/goal-details.component.ts
+++ src/app/components/goal-details/goal-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GoalService } from 'src/app/services/goal.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal-details',
@@ -7,9 +9,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoalDetailsComponent implements OnInit {
 
-  constructor() { }
+  currentGoal = null;
+  message = '';
 
-  ngOnInit(): void {
+  constructor(
+    private goalService: GoalService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+ 
+  ngOnInit() {
+    this.message = '';
+    this.getGoal(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getGoal(id) {
+    this.goalService.get(id)
+      .subscribe(
+        data => {
+          this.currentGoal = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updatePublished(status) {
+    const data = {
+      title: this.currentGoal.title,
+      description: this.currentGoal.description,
+      published: status
+    };
+
+    this.goalService.update(this.currentGoal.id, data)
+      .subscribe(
+        response => {
+          this.currentGoal.published = status;
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  updateGoal() {
+    this.goalService.update(this.currentGoal.id, this.currentGoal)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = 'The goal was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  deleteGoal() {
+    this.goalService.delete(this.currentGoal.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/goals']);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
