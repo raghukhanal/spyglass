8d3c0fd9fcf4db21906ab431b546cb50c899db19
micelide <dm1053@scarletmail.rutgers.edu>
2021-05-19 16:17:55 -0400

    fixed the date bug!!!!!!! WOOOOOOOOOO

--- src/app/components/goal-list/goal-list.component.ts
+++ src/app/components/goal-list/goal-list.component.ts
@@ -17,18 +17,13 @@ export class GoalListComponent implements OnInit {
   totalMonthsLeft = 0;
   recommendedAmountPerMonth = 0;
   message = '';
- 
- 
-
   //for getting recommended amount to be funded per month
   targetAmount: number;
   targetDate: Date;
   currentAmount: number;
   todaysDate: Date = new Date();
-
   //Need a method to calculate percent of goal acheived 
   percentage = 0;
-
 
   constructor(
     private goalService: GoalService,
@@ -40,7 +35,6 @@ export class GoalListComponent implements OnInit {
     this.retrieveGoals();
     //this.recommendedMonthlyPayment();
   }
-
   retrieveGoals() {
     this.goalService.getAll()
       .subscribe(
@@ -63,24 +57,22 @@ export class GoalListComponent implements OnInit {
           console.log(error);
         });
   }
-
-
-// ******************************* TO DO: ****************************************** //
 recommendedMonthlyPayment(id) {
   //totalAmount - currentAmount / totalMonthsLeft
   this.goalService.get(id)
     .subscribe(
       data => {
         this.currentGoal = data;
-        console.log(data);
+       // console.log(data);
         var now = new Date();
         var target = new Date(data.targetDate);
-        console.log("Target: " + target);
+      //  console.log("Target: " + target);
+      //calculate the total number of months in the goal, then calculate the number of months left
         var yearsLeftWithMonth = (target.getFullYear() - now.getFullYear()) * 12;
         var monthsLeft = target.getMonth() - now.getMonth();
         this.totalMonthsLeft = monthsLeft + yearsLeftWithMonth;
-        console.log("Months left: "+ this.totalMonthsLeft);
-        
+      //  console.log("Months left: "+ this.totalMonthsLeft);
+        //error check before calculating the recommended payment
         if(this.totalMonthsLeft == 0) {
           this.recommendedAmountPerMonth = data.targetAmount - data.currentAmount;
         } else if(this.totalMonthsLeft < 0) {
@@ -89,52 +81,35 @@ recommendedMonthlyPayment(id) {
         } else {
           this.recommendedAmountPerMonth = (data.targetAmount - data.currentAmount)/this.totalMonthsLeft;
         }
-       
-
-      //  console.log("$$$$: "+ this.recommendedAmountPerMonth);
       },
       error => {
         console.log(error);
       });
-
+}
+refreshList() {
+  this.retrieveGoals();
+  this.currentGoal = null;
+  this.currentIndex = -1;
 }
 
+setActiveGoal(goal, index) {
+  this.currentGoal = goal;
+  this.currentIndex = index;
+  this.recommendedMonthlyPayment(goal.id);
+}
 
-    
-    // targetAmount  
-
-    //targetDate 
-    //Today's date 
-    // extract# of months left
-  
-
-  // ********************************************************************************************** //
-
-
-  refreshList() {
-    this.retrieveGoals();
-    this.currentGoal = null;
-    this.currentIndex = -1;
+removeAllGoals() {
+  this.goalService.deleteAll()
+    .subscribe(
+      response => {
+        console.log(response);
+        this.retrieveGoals();
+      },
+      error => {
+        console.log(error);
+      });
   }
 
-  setActiveGoal(goal, index) {
-    this.currentGoal = goal;
-    this.currentIndex = index;
-    this.recommendedMonthlyPayment(goal.id);
-  }
-
-  removeAllGoals() {
-    this.goalService.deleteAll()
-      .subscribe(
-        response => {
-          console.log(response);
-          this.retrieveGoals();
-        },
-        error => {
-          console.log(error);
-        });
-    }
-
-  }
+}
 
 
